470b64b2d599de3a8801904cfe32b08390970454
raghavanand98 <raghav_anand98@yahooc=.com>
2017-01-22 07:12:47 -0800

    Got graph working

--- this_time_flask/static/dist/main.js
+++ this_time_flask/static/dist/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     idTwo = secondLastClicked.attr('id');
     getJsonData(idOne, idTwo);
   });
-    
+
 
 });
 
@@ -44,70 +44,107 @@ var populateGraphs = function(data1, data2, attribute1, attribute2) {
   for(var label in data1) {
     labels.push(label);
   }
-  var data = { labels: labels };
+  var data = { } 
   var innerData = [];
   for(var label of labels){
     innerData.push({
-      x: data1[label],
-      y: data2[label]
+      data: [{
+        x: data1[label],
+        y: data2[label],
+      }],
+      label: label,
+      strokeColor: '#FFF'
     });
   }
-  //innerData.sort(compare);
+  innerData.sort(compare);
   console.log(innerData);
-  data.datasets = [{
-    backgroundColor: "rgba(153, 255, 51, 0.5)",
-    data: innerData
-  }];
+    data.datasets = [{
+      backgroundColor: "rgba(153, 255, 51, 0.5)",
+      data: innerData
+    }];
   //console.log(labels)
   //console.log(data);
   $('#myChart').remove();
   $('.append-chart-to').append(' <canvas id="myChart"></canvas>');
-  createTwoFeature(data, attribute1, attribute2, labels);
+  createTwoFeature(innerData, attribute1, attribute2, labels);
 }
 
 function compare(a,b) {
-    if (a.y < b.y )
-          return -1;
-      if (a.y > b.y)
-            return 1;
-        return 0;
+  if (a.data.x < b.data.x )
+    return -1;
+  if (a.data.x > b.data.x)
+    return 1;
+  return 0;
 }
 
 
-var createTwoFeature = function(data, xLabel, yLabel, labels) {
+var createTwoFeature = function(innerData, xLabel, yLabel, labels) {
   var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+  /*
+     var myChart = new Chart(ctx, {
+     type: 'line',
+     data: data,
+     options:
+     {
+     legend: {
+     display: false
+     },
+     scales:
+     {
+     yAxes: [{
+     display: true,
+     ticks: {
+     suggestedMin: 0,    
+     beginAtZero: true   // minimum value will be 0.
+     },
+     scaleLabel: {
+     display: true,
+     labelString: yLabel
+     }
+     }],
+     xAxes: [{
+     display: false,
+     ticks: {
+     suggestedMin: 0,    
+     beginAtZero: true   // minimum value will be 0.
+     },
+     scaleLabel: {
+     display: true,
+     labelString: xLabel ,
+     }
+     }],
+     }
+     }
+     });
+     */
+  console.log(innerData)
+  var scatterChart = new Chart(ctx, {
+    backgroundColor: 'white',
     type: 'line',
-    data: data,
-    options:
-    {
+    data: {
+      labels: labels,
+      datasets: innerData    },
+    options: {
       legend: {
         display: false
       },
-      scales:
-      {
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom',
+          scaleLabel: {
+            display: true,
+            labelString: xLabel
+          }
+        }],
         yAxes: [{
-          display: true,
-          ticks: {
-            suggestedMin: 0,    
-            beginAtZero: true   // minimum value will be 0.
-          },
+          type: 'linear',
+          position: 'left',
           scaleLabel: {
             display: true,
             labelString: yLabel
           }
-        }],
-        xAxes: [{
-          display: false,
-          ticks: {
-            suggestedMin: 0,    
-            beginAtZero: true   // minimum value will be 0.
-          },
-          scaleLabel: {
-            display: true,
-            labelString: xLabel ,
-          }
-        }],
+        }]
       }
     }
   });
